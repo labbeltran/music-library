9d5e7dc1a7706c6d65872a89fb9ed8960233747e
labbeltran <labbeltran@gmail.com>
2024-02-25 12:13:07 -0500

    trying to fix spinner

--- music_library/src/components/Spinner.js
+++ music_library/src/components/Spinner.js
@@ -1,9 +1,9 @@
-import load_spinner from './components/loading-buffering.gif'
+import loading from './components/loading-buffering.gif'
 
 const Spinner = () => {
     return (
         <div>
-            <img src={load_spinner} alt="loading..." />
+            <img src={loading} alt="loading..." />
         </div>
     )
 }
